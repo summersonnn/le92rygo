5ae559d9f4c688ccb8d25a45241c0f37ae45e9da
Kubilay Yazoğlu <kyazogluu@gmail.com>
2021-11-11 14:45:54 +0300

    Add files via upload

--- le92rygo/function.py
+++ le92rygo/function.py
@@ -2,10 +2,10 @@ import numpy as np
 from ipywidgets import interact, fixed
 from PIL import Image
 def imshow(X, resize=None):
-"""
-You should create a way to resize an image from an array X.
-The use of widgets is optional but you can take a look to interact.
-We should be able to install this package in Google Colab from your Git
-repo.
-"""
-pass
+    """
+    You should create a way to resize an image from an array X.
+    The use of widgets is optional but you can take a look to interact.
+    We should be able to install this package in Google Colab from your Git
+    repo.
+    """
+    print("Just testing...")
