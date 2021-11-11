5ac8cee2ac9b0113d7c08907041ef0edbf3aaeae
Kubilay Yazoğlu <kyazogluu@gmail.com>
2021-11-11 15:53:12 +0300

    setup.py configured according to my info

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 from setuptools import find_packages
-setup(name='idm_id',
+setup(name='le92rygo',
 version='0.1',
-author='DSSS',
-author_email='name@fau.de',
+author='Kubilay Yazoglu',
+author_email='kubilay.ky.yazoglu@fau.de',
 packages=find_packages(),
 install_requires=['numpy', 'Pillow', 'ipywidgets'])
