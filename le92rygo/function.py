1e26cf2493c208d8d11e7ef43b0ce2470bc6966c
Kubilay Yazoğlu <kyazogluu@gmail.com>
2021-11-12 23:32:31 +0300

    changed nothing

--- le92rygo/function.py
+++ le92rygo/function.py
@@ -3,9 +3,10 @@ from ipywidgets import interact, fixed
 from PIL import Image
 import matplotlib.pyplot as plt
 
+
+
 def imshow(X, resize=None):
     #Convert incoming array to PIL image object
     im = Image.fromarray(X)
-
     #Resize it and show
     plt.imshow( im.resize(resize) )
